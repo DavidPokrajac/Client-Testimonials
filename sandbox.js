45ff8a9733db1f267ff50e0dfec4ce879c87ac8a
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-13 20:22:33 +0200

    Created function constructor and object instances of that constructor

--- sandbox.js
+++ sandbox.js
@@ -18,6 +18,24 @@ window.onload = function(){
   img.id = "thumbnail";
   h3.id = "username";
   evalu.id = "evaluation";
-  p.id = "review";  
+  p.id = "review";
+
+  // Creating a function constructor
+  function Person(image, name, review, evaluation){
+    this.image = `.img/customer-${image}.jpg`;
+    this.name = name;
+    this.review = review;
+    this.evaluation = evaluation;
+  }
+
+  // Creating object instances off of function constructor
+  var personOne = new Person(0, "David", "I liked the atmosphere. Everything seemed the way it should be and the crew was the best one could ever imagine.", "<span><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star'></i></span>");
+  var personTwo = new Person(1, "Kevin", "Kind of disappointed with their service. They didn't offer any help when I called them to say I ran out of gas on the highway.", "<span><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star'></i><i class='fas fa-star'></i></span>");
+  var personThree = new Person(2, "Simon", "I did not get a refund from them. The car tire was slashed and I didn't have back up tire to replace with it, so I had to all a local mechanic to come and tow it to his garage so he could reapir it.", "<span><i class='fas fa-star orange'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i></span>");
+  var personFour = new Person(3, "Mark", "As a long time customer, I can honestly say I have never had any problems with them.", "<span><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i><i class='fas fa-star orange'></i></span>");
+  var personFive = new Person(4, "Michael", "Just scheduled an appointment to them, am totally inexprienced and was told they are the best in the business.", "<span><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i><i class='fas fa-star'></i></span>");
+
+  // Storing object instances in the array
+  arr = [personOne, personTwo, personThree, personFour, personFive];
 
 }
